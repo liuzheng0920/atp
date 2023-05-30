fe5857cd759b73c8cb4c835f216bc825cabf87e1
Liuzheng <liuzheng_0920@126.com>
2023-05-30 18:04:44 +0800

    增加 naiveui 框架

--- atp-vue/src/App.vue
+++ atp-vue/src/App.vue
@@ -19,6 +19,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <n-button type="error">Default</n-button>
       </nav>
     </div>
   </header>
