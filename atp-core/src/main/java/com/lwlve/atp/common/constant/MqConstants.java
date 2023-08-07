9e2b733e4d734647559111546463f0a3b5f3fba7
Liuzheng <liuzheng_0920@126.com>
2023-08-07 15:44:20 +0800

    增加MQ处理

---
+++ atp-core/src/main/java/com/lwlve/atp/common/constant/MqConstants.java
@@ -0,0 +1,20 @@
+package com.lwlve.atp.common.constant;
+
+/**
+ * Mq常量
+ *
+ * @author Liuzheng
+ * @date 2023/8/4 11:20
+ */
+public interface MqConstants {
+    String DEMO_QUEUE_DEMO1 = "demo_queue_demo1";
+
+    String DEMO_QUEUE_DEMO2 = "demo_queue_demo2";
+
+    String DEMO_ROUTE_KEY_DEMO1 = "demo_route_key_demo1";
+
+    String DEMO_ROUTE_KEY_DEMO2 = "demo_route_key_demo2";
+
+    String DEMO_TOPIC_DEMO = "demo_topic_demo";
+
+}
---
+++ atp-system/src/main/java/com/lwlve/atp/config/RabbitMqConfig.java
@@ -0,0 +1,36 @@
+package com.lwlve.atp.config;
+
+import com.lwlve.atp.common.constant.MqConstants;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *  RabbitMq  配置
+ *
+ * @author Liuzheng
+ * @date 2023/8/4 11:18
+ */
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public TopicExchange demoTopicExchange(){
+        return new TopicExchange(MqConstants.DEMO_TOPIC_DEMO);
+    }
+
+    @Bean
+    public Queue demo1Queue(){
+        return new Queue(MqConstants.DEMO_QUEUE_DEMO1);
+    }
+
+    @Bean
+    public Queue demo2Queue(){
+        return new Queue(MqConstants.DEMO_QUEUE_DEMO2);
+    }
+
+
+
+}
