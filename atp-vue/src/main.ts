4df488d86744ea81963319450fbe32a657a53fa3
Liuzheng <liuzheng_0920@126.com>
2023-09-04 15:02:42 +0800

    初始化前段项目

--- atp-vue/src/main.ts
+++ atp-vue/src/main.ts
@@ -1,7 +1,5 @@
 import './assets/main.css'
 
-import 'ant-design-vue/dist/reset.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- atp-vue/src/router/index.ts
+++ atp-vue/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-import HomeView from '@/components/layout/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,23 +7,16 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-      children: [
-        {
-          path: '/',
-          name: 'bpmn',
-          component: () => import('@/components/bpmn/BpmnHome.vue')
-        }
-      ]
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
-    // {
-    //   path: '/about',
-    //   name: 'about'
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   // component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
