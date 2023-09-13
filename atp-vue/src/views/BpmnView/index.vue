718bac53c6107354190bbd988f309f8b0b7f61dd
Liuzheng <liuzheng_0920@126.com>
2023-09-13 09:44:34 +0800

    增加 bpmnn ts类型支持

--- atp-vue/src/views/BpmnView/index.vue
+++ atp-vue/src/views/BpmnView/index.vue
@@ -4,6 +4,7 @@ import 'bpmn-js/dist/assets/bpmn-js.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import '@bpmn-io/properties-panel/assets/properties-panel.css'
 
 import BpmnModeler from 'bpmn-js/lib/Modeler'
 import { queryNewDiagram } from '@/api/bpmn'
@@ -12,13 +13,19 @@ import type { BaseResponse } from '@/types/public/BaseVo'
 import Translate from '@/components/bpmn/additionalModules/Translate'
 import { downloadFile, setEncoded } from '@/utils/files'
 
+import { BpmnPropertiesPanelModule, BpmnPropertiesProviderModule } from 'bpmn-js-properties-panel';
+
 let modeler: BpmnModeler
 
 const canvas = ref<HTMLDivElement | null>(null)
+const panel = ref<HTMLDivElement | null > (null)
 const initModeler = () => {
   modeler = new BpmnModeler({
     container: canvas.value as HTMLDivElement,
-    additionalModules: [Translate]
+    additionalModules: [Translate,BpmnPropertiesPanelModule,BpmnPropertiesProviderModule],
+    propertiesPanel:{
+      parent:panel.value as HTMLDivElement
+    }
   })
   queryNewDiagram().then((response: BaseResponse<string>) => {
     modeler.importXML(response.data)
@@ -45,37 +52,40 @@ onMounted(() => {
 <template>
   <div class="modelerView">
     <div class="modelerView-button">
-
       <a-button type="primary" @click="exportXml">导出Xml</a-button>
-
       <a-button type="primary" @click="exportSvg">导出Svg</a-button>
-
     </div>
-    <div class="canvas" ref="canvas"></div>
+    <div class="main-content">
+      <div class="canvas" ref="canvas"></div>
+      <div class="panel" ref="panel"></div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .modelerView {
+  display: flex;
+  overflow: hidden;
   position: relative;
+  flex-direction: column;
   height: 100%;
   width: 100%;
   .modelerView-button {
-    position: absolute;
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
-    padding: 8px 0;
-    top: 10px;
-    left: 150px;
-    z-index: 1;
-    button{
-      flex: 1;
-    }
-  }
-  .canvas {
-    height: 100%;
     width: 100%;
+    padding: 8px 16px;
+    display: flex;
+    align-items: center;
+  }
+  .main-content {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+  }
+  .canvas{
+    flex: 1;
+  }
+  .panel {
+    width: 480px;
   }
 }
 </style>
